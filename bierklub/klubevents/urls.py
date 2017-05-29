ab46d5f70b80d93177f2151a16ac30d701ca82f6
Hayden Chudy <hjc1710@gmail.com>
2017-05-29 15:46:38 -0400

    Views: Add templates and views for the other major parts of the site.

--- bierklub/klubevents/urls.py
+++ bierklub/klubevents/urls.py
@@ -3,6 +3,17 @@ from django.conf.urls import url
 from . import views
 
 
+app_name = 'klubevents'
 urlpatterns = [
+    # ex: /events/
     url(r'^$', views.index, name='index'),
+    # ex: /events/5/
+    url(r'(?P<event_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /events/5/attending/
+    url(r'(?P<event_id>[0-9]+)/attending/$', views.attending,
+        name='attending'),
+    url(r'(?P<event_id>[0-9]+)/attending/submit/$', views.attending_submit,
+        name='attending_submit'),
+    url(r'(?P<event_id>[0-9]+)/attending/(?P<member_id>[0-9]+)',
+        views.attending_success, name='attending_success'),
 ]
--- bierklub/klubevents/views.py
+++ bierklub/klubevents/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from .models import Event, Member
 
@@ -12,3 +13,52 @@ def index(request):
     }
 
     return render(request, 'events/index.html', context)
+
+
+def detail(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+
+    return render(request, 'events/detail.html', {'event': event})
+
+
+def attending(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+
+    return render(request, 'events/attending.html', {'event': event})
+
+
+def attending_submit(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+
+    email = request.POST.get('email')
+    name = request.POST.get('name')
+
+    if not name or not email:
+        return render(request, 'events/attending.html', {
+            'event': event,
+            'error_message': 'You must fill out all fields.',
+            'name': name,
+            'email': email,
+        })
+
+    try:
+        member = Member.objects.get(email=email)
+    except Member.DoesNotExist:
+        member = Member(email=email, name=name)
+        member.save()
+
+    event.attendees.add(member)
+    event.save()
+
+    return HttpResponseRedirect(reverse('klubevents:attending_success',
+                                        args=(event.id, member.id)))
+
+
+def attending_success(request, event_id, member_id):
+    event = get_object_or_404(Event, pk=event_id)
+    member = get_object_or_404(Member, pk=member_id)
+
+    return render(request, 'events/attending_success.html', {
+        'event': event,
+        'member': member
+    })
