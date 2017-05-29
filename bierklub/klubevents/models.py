74965fa8ca526542a81a1b93856fd2d091619d42
Hayden Chudy <hjc1710@gmail.com>
2017-05-29 12:39:52 -0400

    Models: Add an initial model for event dates and members.

--- bierklub/klubevents/models.py
+++ bierklub/klubevents/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=4096)
+    date = models.DateTimeField()
+    number = models.IntegerField('number IRT total events')
+    location = models.CharField('address of brewery', max_length=128)
+    attendees = models.ManyToManyField('Member',
+                                       db_table='klubevents_event_members')
+
+
+class Member(models.Model):
+    name = models.CharField('full name', max_length=128)
+    email = models.EmailField()
+    join_date = models.DateField()
+
