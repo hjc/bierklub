3f05a098a743050d8418829d60e506b50746a650
Hayden Chudy <hjc1710@gmail.com>
2017-05-29 15:46:12 -0400

    Templates: An initial template for the index page.

--- bierklub/klubevents/views.py
+++ bierklub/klubevents/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Event, Member
 
 
 def index(request):
-    return HttpResponse('Sup.')
+    latest = Event.objects.order_by('-published_date')[:5]
+
+    context = {
+        'latest_event_list': latest,
+    }
+
+    return render(request, 'events/index.html', context)
