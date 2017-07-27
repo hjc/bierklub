f01ec363896da5a484039f1896a8fa0172216edb
Hayden Chudy <hjc1710@gmail.com>
2017-07-27 18:21:49 -0400

    Sanity: Error handlers should return the right error code.
    Thought Django would do this, but apparently not.
    
    I hate Django.

--- bierklub/error_handlers/views.py
+++ bierklub/error_handlers/views.py
@@ -3,13 +3,17 @@ from django.shortcuts import render
 
 def standard_404(request):
     """Return a standard Page Not Found."""
-    return render(request, "error_handlers/404.html", {'title': 'Page Not Found'})
+    resp = render(request, "error_handlers/404.html", {'title': 'Page Not Found'})
+    resp.status_code = 404
+    return resp
 
 
 def standard_500(request):
     """Return a standard 500 page."""
-    return render(
+    resp = render(
         request,
         'error_handlers/500.html',
         {'title': 'Uh Oh!'}
     )
+    resp.status_code = 500
+    return resp
